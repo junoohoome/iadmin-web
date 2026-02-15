630dfc1081844ff38298aa2989e636794894f255
johnnyfong <johnnyfong0430@gmail.com>
2026-02-15 23:23:23 +0800

    fix: 恢复完整首页 Dashboard 组件

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,17 +1,93 @@
 <template>
   <div class="dashboard-editor-container">
-    <h1>欢迎使用 IAdmin 后台管理系统</h1>
+    <panel-group @handle-set-line-chart-data="handleSetLineChartData" />
+
+    <el-row style="background: #fff; padding: 16px 16px 0; margin-bottom: 32px">
+      <line-chart :chart-data="currentLineChartData" />
+    </el-row>
+
+    <el-row :gutter="32">
+      <el-col :xs="24" :sm="24" :lg="8">
+        <div class="chart-wrapper">
+          <raddar-chart />
+        </div>
+      </el-col>
+      <el-col :xs="24" :sm="24" :lg="8">
+        <div class="chart-wrapper">
+          <pie-chart />
+        </div>
+      </el-col>
+      <el-col :xs="24" :sm="24" :lg="8">
+        <div class="chart-wrapper">
+          <bar-chart />
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script setup lang="ts">
-// 简化首页，排除 dashboard 组件的问题
+import { ref } from 'vue'
+import PanelGroup from './dashboard/components/PanelGroup.vue'
+import LineChart from './dashboard/components/LineChart.vue'
+import RaddarChart from './dashboard/components/RaddarChart.vue'
+import PieChart from './dashboard/components/PieChart.vue'
+import BarChart from './dashboard/components/BarChart.vue'
+
+interface LineChartData {
+  expectedData: number[]
+  actualData: number[]
+}
+
+interface LineChartDataMap {
+  newVisitis: LineChartData
+  messages: LineChartData
+  purchases: LineChartData
+  shoppings: LineChartData
+}
+
+const lineChartData: LineChartDataMap = {
+  newVisitis: {
+    expectedData: [100, 120, 161, 134, 105, 160, 165],
+    actualData: [120, 82, 91, 154, 162, 140, 145]
+  },
+  messages: {
+    expectedData: [200, 192, 120, 144, 160, 130, 140],
+    actualData: [180, 160, 151, 106, 145, 150, 130]
+  },
+  purchases: {
+    expectedData: [80, 100, 121, 104, 105, 90, 100],
+    actualData: [120, 90, 100, 138, 142, 130, 130]
+  },
+  shoppings: {
+    expectedData: [130, 140, 141, 142, 145, 150, 160],
+    actualData: [120, 82, 91, 154, 162, 140, 130]
+  }
+}
+
+const currentLineChartData = ref<LineChartData>(lineChartData.newVisitis)
+
+function handleSetLineChartData(type: keyof LineChartDataMap) {
+  currentLineChartData.value = lineChartData[type]
+}
 </script>
 
 <style lang="scss" scoped>
 .dashboard-editor-container {
   padding: 32px;
   background-color: rgb(240, 242, 245);
-  min-height: calc(100vh - 50px);
+  position: relative;
+
+  .chart-wrapper {
+    background: #fff;
+    padding: 16px 16px 0;
+    margin-bottom: 32px;
+  }
+}
+
+@media (max-width: 1024px) {
+  .chart-wrapper {
+    padding: 8px;
+  }
 }
 </style>
